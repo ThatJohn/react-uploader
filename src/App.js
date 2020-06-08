ae63ec8e29fcb7caa728532180153e9059a15255
ThatJohn <fajardocj@gmail.com>
2020-06-08 14:28:42 -0400

    Adds upload form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Create a new post</h1>
+      <hr/>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <div className="post-title">
+            <label htmlFor="title">Post title: </label>
+            <input name="title" type="text" onChange={this.handleChange} value={this.state.title} />
+          </div>
+          <div className="post-body">
+            <label htmlFor="body">Post body</label>
+            <textarea name="body" onChange={this.handleChange} value={this.state.body} />
+          </div>
+          <div className="post-image">
+            <label htmlFor="img">Select image:</label>
+            <input type="file" accept="image/*" multiple={false} onChange={this.onImageChange} />
+          </div>
+          <button className="submit" type="submit">Create post</button>
+        </form>
+      </div>
+      {this.state.posts.map(post => (
+        <Post key={post.id} title={post.title} body={post.body} url={post.featured_image.url} />
+      ))}
     </div>
   );
 }
